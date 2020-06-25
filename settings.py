05317f96bf51e922ceff26945d1482064d2415c0
Guillermo Tomac <guillermotomac@gmail.com>
2020-06-25 23:49:59 +0100

    Bullet created

--- settings.py
+++ settings.py
@@ -8,5 +8,11 @@ class Settings:
         self.screen_height = 700
         self.bg_color = (230, 230, 230)
 
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+
         # Ship settings
         self.ship_speed = 1.5
